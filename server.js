838e380ee66178dcf8ff9b697b41121ad4f015d0
Mário Brandão <mariobrandaobr@gmail.com>
2025-02-10 18:11:58 -0300

    build: bundle decrease attempt

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
 
+const compression = require("compression");
 const app = express();
+app.use(compression());
 const options = {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.crt'),
+  key: fs.readFileSync("./server.key"),
+  cert: fs.readFileSync("./server.crt"),
 };
 
-app.get('/', (req, res) => {
-    res.send('Hello, HTTPS World!');
+app.get("/", (req, res) => {
+  res.send("Hello, HTTPS World!");
 });
 
 https.createServer(options, app).listen(443, () => {
-    console.log('Servidor rodando em https://localhost:443');
+  console.log("Servidor rodando em https://localhost:443");
 });
