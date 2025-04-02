e37d62f05b0dca4282562ac991131a1c7eeaede6
Mário Brandão <mariobrandaobr@gmail.com>
2025-04-02 18:00:22 -0300

    feat: loading spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoadingOverlayComponent } from './loading-overlay/loading-overlay.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 @NgModule({
-  declarations: [LoadingOverlayComponent],
+  declarations: [LoadingOverlayComponent, LoadingSpinnerComponent],
   imports: [CommonModule],
-  exports: [LoadingOverlayComponent],
+  exports: [LoadingOverlayComponent, LoadingSpinnerComponent],
 })
 export class SharedModule {}
