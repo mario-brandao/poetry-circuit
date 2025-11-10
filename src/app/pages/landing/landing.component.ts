71e844e4c9257167485bd2925afe2d931387c869
João Vitor Oliveira <jvoa@cesar.org.br>
2025-11-10 11:58:38 -0300

    Adding google login

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { db } from 'src/db';
 
+declare var google: any;
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -9,6 +11,33 @@ import { db } from 'src/db';
 })
 export class LandingComponent {
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    window.onload = () => {
+      this.initializeGoogleSignIn();
+    };
+  }
+
+  initializeGoogleSignIn(): void {
+    google.accounts.id.initialize({
+      client_id:
+        '289710175954-dp3isc2ufmr7lg8mbf6fka903ellc5i5.apps.googleusercontent.com',
+      callback: (response: any) => this.handleCredentialResponse(response),
+    });
+
+    google.accounts.id.renderButton(document.getElementById('googleBtn'), {
+      type: 'standard',
+    });
+  }
+
+  handleCredentialResponse(response: any): void {
+    const token = response.credential;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+
+    console.log('Google UID:', payload.sub);
+    console.log('Nome:', payload.name);
+    console.log('Email:', payload.email);
+  }
 
   async toFirstAccess(): Promise<void> {
     const user = await db.user.get(1);
@@ -18,4 +47,6 @@ export class LandingComponent {
     }
     this.router.navigate(['/home']);
   }
+
+  onGoogleLoginClicked(): void {}
 }
